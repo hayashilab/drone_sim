2bd15cdf40ad6ee4a8ba9224b502d327c745a6cb
titangil <86349365+titangil@users.noreply.github.com>
2023-08-22 16:50:27 +0900

    Delete launch_stil.py

--- scripts/launch_stil.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-import subprocess
-subprocess.call(["sh","/home/titan/drone_ws/src/drone_sim/scripts/launch_stil_.sh"])
-#("/home/titan/drone_ws/src/drone_sim/scripts/launch_stil_.sh") 
