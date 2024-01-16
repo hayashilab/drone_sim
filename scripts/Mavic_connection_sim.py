293aceb0072264e4d497ffa230e3535ea88ccfdf
titangil <86349365+titangil@users.noreply.github.com>
2024-01-16 22:37:54 +0900

    Update Mavic_connection_sim.py

--- scripts/Mavic_connection_sim.py
+++ scripts/Mavic_connection_sim.py
@@ -16,63 +16,18 @@ def send_local_velocity( vx, vy, vz,yaw):
         0,float(yaw))  # yaw rate in radians/s...  
     vehicle.send_mavlink(msg)
     vehicle.flush()
-def keyboard_cmd(speed):
-    event = keyboard.read_event()
-    
-    if event.event_type == keyboard.KEY_DOWN:
-        
-        if event.name == str(8):send_local_velocity    (speed+0.1   , 0     , 0     ,0    ); command="forward"
-        elif event.name == str(2):send_local_velocity  (-0.3  , 0     , 0     ,0    ); command="backward"
-        elif event.name == str(6):send_local_velocity  (0     , 0.3   , 0     ,0    ); command="left"
-        elif event.name == str(4):send_local_velocity  (0     , -0.3  , 0     ,0    ); command="right"
-        elif event.name == str(1):send_local_velocity  (0     , 0     , 0.1   ,0    ); command="down"
-        elif event.name == str(3):send_local_velocity  (0     , 0     , -0.1  ,0    ); command="up"
-        elif event.name == str(7):send_local_velocity  (0     , 0     , 0     ,-0.3 ); command="cw"
-        elif event.name == str(9):send_local_velocity  (0     , 0     , 0     ,0.3  ); command="ccw"
-        # elif event.name == str(8):velocity= (0      ,0      ,0      ,0)
-        # elif event.name == str(8):velocity= (0      ,0      ,0      ,0)
-    else :send_local_velocity (0,0,0,0); command="stop"
-    #if event.event_type == keyboard.KEY_DOWN and event.name == str(event):velocity=(0.3,0,0,0)
-    
-    return command
-    # if event.event_type == keyboard.KEY_DOWN:
-    #     if event.name == str(8):send_local_velocity    (0.3   , 0     , 0     ,0    ); command="forward"
-    #     elif event.name == str(2):send_local_velocity  (-0.3  , 0     , 0     ,0    ); command="backward"
-    #     elif event.name == str(6):send_local_velocity  (0     , 0.3   , 0     ,0    ); command="left"
-    #     elif event.name == str(4):send_local_velocity  (0     , -0.3  , 0     ,0    ); command="right"
-    #     elif event.name == str(1):send_local_velocity  (0     , 0     , 0.1   ,0    ); command="down"
-    #     elif event.name == str(3):send_local_velocity  (0     , 0     , -0.1  ,0    ); command="up"
-    #     elif event.name == str(7):send_local_velocity  (0     , 0     , 0     ,-0.3 ); command="cw"
-    #     elif event.name == str(9):send_local_velocity  (0     , 0     , 0     ,0.3  ); command="ccw"
-    #     # elif event.name == str(8):velocity= (0      ,0      ,0      ,0)
-    #     # elif event.name == str(8):velocity= (0      ,0      ,0      ,0)
-    # else:send_local_velocity (0,0,0,0); command="stop"
-    # #if event.event_type == keyboard.KEY_DOWN and event.name == str(event):velocity=(0.3,0,0,0)
-    
-    # return command
+
 
 connection_string = 'udp:0.0.0.0:14550'
 print("\nConnecting to vehicle on: %s" % connection_string)
 vehicle = connect(connection_string, wait_ready=False)
 print("Connected!")
 
-speed = 0
+
 
 while True:
         try:
-             send_local_velocity (0.3,0,0,0)
-            # key = keyboard_cmd(speed)
-            # if speed>= 0.3:speed=0.3
-            # if key == "stop": speed-=0.1;print("STOP")
-            # else:speed+=0.01
-            
-            
-            
-            
-            
-            #print(key)
-            
-           
+            send_local_velocity (0,0,0,0)
             # event = keyboard.read_event()
             # if event.event_type == keyboard.KEY_DOWN and event.name == '8':
             #     print('space was pressed,forward')
